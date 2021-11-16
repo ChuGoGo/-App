c5606ecaeba272530ed57bf16dcfadaa863435ec
chugogo <ioueasonae86@yahoo.com.tw>
2021-11-16 15:45:27 +0800

    Initial Commit

---
+++ 大友克洋App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  大友克洋App
+//
+//  Created by Chu Go-Go on 2021/11/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
